4be3ccf90603b34cc8c60f260a058b08f1ae0c55
beaesreal <beaesreal@gmail.com>
2022-10-18 14:18:29 +0000

    Cards added

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -1,4 +1,45 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import PropType from "prop-types";
+
+const Card = (props) => {
+	// 1) replace the hard-coded image, description, link, etc. With their property variable.
+	return (
+        <div className="container m-0 p-0">
+
+        <div className="card text-center">
+        <img className="card-img-top" src={props.imageUrl} alt="Card image cap"></img>
+        <div className="card-body py-4">
+            <h5 className="card-title">{props.title}</h5>
+            <p className="card-text">{props.description}</p>
+
+        </div>
+        <div className="card-footer p-2">
+            <a href="#" className="btn btn-primary p-2">Find Out More!</a>
+            </div>
+        </div>
+
+        
+
+    </div>
+
+
+
+	);
+};
+Card.propTypes = {
+    //add pproperties
+	title: PropType.string,
+	imageUrl: PropType.string,
+	description: PropType.string,
+	buttonUrl: PropType.string,
+	buttonLabel: PropType.string,
+};
+
+
+export default Card;
+
+/* import React from "react";
 
 const Card = () => {
   return (
@@ -36,4 +77,18 @@ const Card = () => {
   );
 }
 
-export default Card;
+export default Card; 
+
+
+
+
+<div className="card m-5">
+<img className="card-img-top" src={props.imageUrl} alt="Card image cap" />
+<div className="card-body">
+    <h5 className="card-title">{props.title}</h5>
+    <p className="card-text">{props.description}</p>
+    <a href={props.buttonUrl} className="btn btn-primary">
+        {props.buttonLabel}
+    </a>
+</div>
+</div> */
--- src/js/index.js
+++ src/js/index.js
@@ -14,5 +14,48 @@ import Card from "./component/card.jsx";
 //render your react application
 ReactDOM.render(<Navbar />, document.querySelector("#navbar"));
 ReactDOM.render(<Jumbotron />, document.querySelector("#jumbotron"));
-ReactDOM.render(<Card />, document.querySelector("#card"));
 
+ReactDOM.render(
+	<Card
+		title="Bob Dylan"
+		imageUrl="https://via.placeholder.com/500x325.png"
+		buttonLabel="Go to wikipedia"
+		buttonUrl="https://en.wikipedia.org/wiki/Bob_Dylan"
+		description="Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter."
+	/>,
+	document.querySelector("#card0")
+);
+
+
+ReactDOM.render(
+	<Card
+		title="Bob Dylan"
+		imageUrl="https://via.placeholder.com/500x325.png"
+		buttonLabel="Go to wikipedia"
+		buttonUrl="https://en.wikipedia.org/wiki/Bob_Dylan"
+		description="Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter."
+	/>,
+	document.querySelector("#card1")
+);
+
+ReactDOM.render(
+	<Card
+		title="Bob Dylan"
+		imageUrl="https://via.placeholder.com/500x325.png"
+		buttonLabel="Go to wikipedia"
+		buttonUrl="https://en.wikipedia.org/wiki/Bob_Dylan"
+		description="Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter."
+	/>,
+	document.querySelector("#card2")
+);
+
+ReactDOM.render(
+	<Card
+		title="Bob Dylan"
+		imageUrl="https://via.placeholder.com/500x325.png"
+		buttonLabel="Go to wikipedia"
+		buttonUrl="https://en.wikipedia.org/wiki/Bob_Dylan"
+		description="Bob Dylan (born Robert Allen Zimmerman, May 24, 1941) is an American singer-songwriter."
+	/>,
+	document.querySelector("#card3")
+);
