eacab3ed24e6fa7e0180d423c24a89f915af997c
beaesreal <beaesreal@gmail.com>
2022-10-18 15:37:29 +0000

    Exercise completed with all functionalities

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -3,7 +3,7 @@ import React from "react";
 //create your first component
 const Jumbotron = () => {
 	return (
-        <div className="container p-4 jumbotroncss">
+        <div className="container p-4 jumbotroncss ">
         <div className="jumbotron">
         <h1 className="display-4">
             A Warm Welcome!
--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -1,30 +1,27 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 const Navbar = () => {
 	return (
-		<div className="bg-dark">
-			<nav class="navbar navbar-expand-lg navbar-dark bg-dark container">
-			<a class="navbar-brand" href="#">Start bootstrap</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
+		<div classNameName="bg-dark">
+			<nav className="navbar navbar-expand-lg navbar-dark bg-dark container">
+			<a className="navbar-brand" href="#">Start bootstrap</a>
+			<button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+				<span className="navbar-toggler-icon"></span>
 			</button>
-			<div class="collapse navbar-collapse" id="navbarText">
-				<ul class="navbar-nav ms-auto">
-				<li class="nav-item active">
-					<a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+			<div className="collapse navbar-collapse" id="navbarText">
+				<ul className="navbar-nav ms-auto">
+				<li className="nav-item active ">
+					<a className="nav-link active" href="#">Home</a>
 				</li>
-				<li class="nav-item ms-auto">
-					<a class="nav-link" href="#">About</a>
+				<li className="nav-item">
+					<a className="nav-link" href="#">About</a>
 				</li>
-				<li class="nav-item ms-auto">
-					<a class="nav-link" href="#">Services</a>
+				<li className="nav-item">
+					<a className="nav-link" href="#">Services</a>
 				</li>
-				<li class="nav-item ms-auto">
-					<a class="nav-link" href="#">Contact</a>
+				<li className="nav-item">
+					<a className="nav-link" href="#">Contact</a>
 				</li>
 				</ul>
 			</div>
